7d60d4b6401bcd60e82ef9e87373b248442af754
Ahmedbilal-s <mech.ahmedbilal@gmail.com>
2024-02-17 18:46:55 +0530

    Added

---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../content.css';
+import MovieListContainer from '../App.js'
+
+
+
+const Content = () => {
+  return (
+    <MovieListContainer>
+     <img src="/movie-icon.svg" alt="ima" className="content"/>
+    </MovieListContainer>
+     
+  
+  )
+}
+
+export default Content
